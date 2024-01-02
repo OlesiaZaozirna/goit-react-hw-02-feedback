d84443b376ba97ed3d62801d62941d01374e5495
OlesiaZaozirna <137696365+OlesiaZaozirna@users.noreply.github.com>
2024-01-02 23:19:59 +0200

    commit 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import css from './App.module.css';
+
+
+class App extends Component{
+
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+  }
+   onLeaveFeedback = type => {
+    this.setState(prevState => ({ [type]: prevState[type] + 1 }));
+  };
+  
+  countTotalFeedback=()=>{ 
+  const { good, neutral, bad } = this.state;
+return good + neutral + bad;
+}
+  
+  countPositiveFeedbackPercentage = () => {
+  const { good } = this.state;
+  const total = this.countTotalFeedback();
+  return total > 0 ? Math.round((good / total) * 100) : 0;
+}
+  
+  render() {
+
+    return (
+       <div className={css.App__container}>
+      <Section title="Your Opinion Matters To Us">
+         <p>Please Leave Us Your Review </p>
+          <FeedbackOptions/>
+        </Section>
+        <Section>
+
+        </Section>
+      </div>
+
+    
+  ); 
+  }
 };
+  
+export default App;
